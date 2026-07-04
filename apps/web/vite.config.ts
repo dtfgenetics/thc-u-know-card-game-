c41f196459b956cf56f90f1dcf2c4b8cf622ee9e
dtfgenetics <green801028@gmail.com>
2026-07-04 15:14:08 -0500

    Simplify vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig, type ConfigEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(function configureVite(env: ConfigEnv) {
-  const base = process.env.VITE_BASE_PATH || (env.command === 'build' ? '/games/thc-u-know/' : '/');
+const basePath = '/games/' + 'thc-u-know/';
 
-  return {
-    base,
-    plugins: [react()],
-    server: {
-      port: 5173
-    }
-  };
+export default defineConfig({
+  base: basePath,
+  plugins: [react()],
+  server: {
+    port: 5173
+  }
 });
