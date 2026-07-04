4664ec0a610209a0268ff96ea2b33f3782579940
dtfgenetics <green801028@gmail.com>
2026-07-04 15:13:14 -0500

    Type Vite config environment parameter

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(function configureVite(env) {
+export default defineConfig(function configureVite(env: ConfigEnv) {
   const base = process.env.VITE_BASE_PATH || (env.command === 'build' ? '/games/thc-u-know/' : '/');
 
   return {
